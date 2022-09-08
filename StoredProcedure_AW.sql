ff97e5b6cd7eb5565c98fb88ef0b68a9a963c213
Thomas-Kulch <54332517+Thomas-Kulch@users.noreply.github.com>
2022-09-08 18:23:22 -0400

    Rename SQLQuery4.sql to StoredProcedure_AW.sql

---
+++ StoredProcedure_AW.sql
@@ -0,0 +1,33 @@
+﻿USE [AdventureWorks2017]
+GO
+/****** Object:  StoredProcedure [dbo].[spTotalSalesByProduct]    Script Date: 2/13/2020 8:58:44 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+--Test Code: [dbo].[spTotalSalesByProduct] '01/01/2013', '12/31/2013'
+-- =============================================
+ALTER PROCEDURE [dbo].[spTotalSalesByProduct] 
+	@startdate datetime, @enddate datetime, @Product VARCHAR(MAX)
+AS
+BEGIN
+	
+	select	distinct prod.ProductID, 
+			prod.name as ProductName,
+			sod.UnitPrice,
+			sod.OrderQty,
+			sod.ModifiedDate
+
+	from [Sales].[SalesOrderDetail] as SOD
+
+	inner join [Production].[Product] as Prod
+
+	on SOD.ProductID = Prod.ProductID
+	where sod.ModifiedDate between @startdate and @enddate
+	and Prod.Name in (@Product)
+
+END
